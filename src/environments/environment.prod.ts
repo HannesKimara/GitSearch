5f3a42788a4d93f736828647aa8229156189f6e1
Hannes Kimara <hanneskimara@gmail.com>
2020-01-20 14:13:41 +0300

    Add Access Keys to Production Environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://api.github.com/user',
+  SEARCH_URL: 'https://api.github.com/search/',
+  API_KEY: '4b3579afcece7b6076ab88364229752712e0f80c'
 };
