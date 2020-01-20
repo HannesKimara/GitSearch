f3f63e2f6927792018ea8b28ca75e73cf9d6f1b1
Hannes Kimara <hanneskimara@gmail.com>
2020-01-20 14:01:55 +0300

    Display Repositories on route

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-details',
@@ -7,13 +9,24 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-
-  constructor(private route: ActivatedRoute) { }
+  repos:any;
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       const username = params.username;
-      console.log(username);
+
+      const promise = new Promise((resolve, reject) => {
+        this.http.get(`${environment.BASE_URL}s/${username}/repos`, {headers: { Authorization: `Bearer ${environment.API_KEY}`}}).toPromise().then(response => {
+          this.repos = response;
+          console.log(this.repos);
+          resolve();
+        },
+        error => {
+        console.log('ERROR OCCURRED');
+        reject(error);
+        });
+      })
   }
     )}
 }
